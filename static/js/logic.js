cc27e16f285592f9ad64c97e9f2ba17287018278
Daniel <vdaniel1925@gmail.com>
2021-12-18 19:22:20 -0600

    multipe

--- static/js/logic.js
+++ static/js/logic.js
@@ -19,7 +19,7 @@ let map = L.map("mapid", {
 }).addTo(map);
 
 //  Add a marker to the map for Los Angeles, California.
-//let marker = L.marker([34.0522, -118.2437]).addTo(map);
+let marker = L.marker([34.0522, -118.2437]).addTo(map);
 
 L.circleMarker([34.0522, -118.2437],{
 
