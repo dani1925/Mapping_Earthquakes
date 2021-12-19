3efdd8bd1c3bf1e947ca113de7d58c202efc579a
Daniel <vdaniel1925@gmail.com>
2021-12-18 18:59:51 -0600

    Single point

---
+++ static/js/config.js
@@ -0,0 +1,1 @@
+const API_KEY ="pk.eyJ1IjoiZGFuaTE5MjUiLCJhIjoiY2t4Y2RlYjlwMzU2aDJvcG1zZ2VrYWs4bSJ9.HoFzVZgZBOxteRZ3ZJJLsg"
---
+++ static/js/logic.js
@@ -0,0 +1,24 @@
+// Add console.log to check to see if our code is working.
+console.log("working");
+console.log(API_KEY);
+// Create the map object with a center and zoom level.
+let map = L.map("mapid", {
+    center: [
+      40.7, -94.5
+    ],
+    zoom: 4
+  });
+
+  var streets =L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: API_KEY
+}).addTo(map);
+
+var marker = L.marker([51.5, -0.09]).addTo(map);
+
+
+
