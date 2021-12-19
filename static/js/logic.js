2a4447733ae321476d10e5a9a31fdd2be745fad0
Daniel <vdaniel1925@gmail.com>
2021-12-18 19:14:04 -0600

    change color

--- static/js/logic.js
+++ static/js/logic.js
@@ -12,13 +12,20 @@ let map = L.map("mapid", {
   var streets =L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox/streets-v11',
+    id: 'mapbox/dark-v10',
     tileSize: 512,
     zoomOffset: -1,
     accessToken: API_KEY
 }).addTo(map);
 
-var marker = L.marker([51.5, -0.09]).addTo(map);
+//  Add a marker to the map for Los Angeles, California.
+//let marker = L.marker([34.0522, -118.2437]).addTo(map);
+
+L.circleMarker([34.0522, -118.2437],{
+
+  radius: 300,
+  color:"black"
+}).addTo(map);
 
 
 
