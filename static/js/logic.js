801f01c9329ccde555d873bc7f3f4b5ecaf7e34f
Daniel <vdaniel1925@gmail.com>
2021-12-18 19:39:23 -0600

    Second Commit

--- static/js/logic.js
+++ static/js/logic.js
@@ -18,14 +18,19 @@ let map = L.map("mapid", {
     accessToken: API_KEY
 }).addTo(map);
 
-//  Add a marker to the map for Los Angeles, California.
-let marker = L.marker([34.0522, -118.2437]).addTo(map);
+// Get data from cities.js
+let cityData = cities;
 
-L.circleMarker([34.0522, -118.2437],{
+// Loop through the cities array and create one marker for each city.
+cityData.forEach(function(city) {
+  console.log(city)
+  L.circleMarker(city.location,{
+    radius: city.population/100000,
+    color:"yellow"
 
-  radius: 300,
-  color:"black"
-}).addTo(map);
-
+  })
+  .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+.addTo(map);
+});
 
 
